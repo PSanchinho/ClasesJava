de8c4c1bd358c3563f27bd9fbb03aab245c7f6ac
psanchino <psanchino@11LAP97PRPN3.indra.es>
2022-07-27 11:09:00 +0200

    desarrollo

--- Sesion11/src/Animo.java
+++ Sesion11/src/Animo.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 
 public class Animo {
-
+	public static void main (String[] args) {
+		System.out.println("Buenas, como se siente hoy? (alegre/triste/nervioso)");
+		String estado;
+		Scanner s = new Scanner(System.in);
+		estado = s.nextLine();
+		
+		switch(estado) {
+			case "alegre": 
+				System.out.println("Enhorabuena!");
+				break;
+			
+			case "triste":
+				System.out.println("Cual es el colmo de Batman? Que le Robin");
+				break;
+				
+			case "nervioso":
+				System.out.println("Eso también puede ocurrir");
+				break;
+				
+			default:
+				System.out.println("Lo siento, no soy especialista en emociones humanas");
+		}
+		
+		
+	}
 }
